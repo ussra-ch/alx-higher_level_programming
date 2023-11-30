d49ed2640726f54b8f900936b52ce60ed995bdfd
zakaria chatouane <yousra.ch.etudiante@gmail.com --global user.name yousra>
2023-11-30 09:57:50 +0100

    all

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,6 @@
-import add_0
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    length = len(sys.argv)
+    result = 0
+    for i in range(1, length):
+        result += int(sys.argv[i])
+    print("{:d}".format(result))
+        
