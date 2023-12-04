75e88f08ed5962d5d9cbc10f68bf918bf214be23
zakaria chatouane <yousra.ch.etudiante@gmail.com --global user.name yousra>
2023-12-05 00:48:06 +0100

    all

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -9,5 +9,3 @@ def replace_in_list(my_list, idx, element):
             if i == idx:
                 my_list[i] = element
                 return (my_list)
-            else:
-                continue
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    a = my_list
-    if idx < 0:
-        return (a)
-    elif idx > len(my_list):
-        return (a)
-    else :
-        for i in range(len(my_list)):
-            if i == idx:
-                a[i] = element
-                return (a)
-            else:
-                continue
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
+
+    cp = [x for x in my_list]
+    cp[idx] = element
+    return (cp)
+
