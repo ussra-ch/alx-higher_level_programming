3ba8c62a9875440d383ab91d697fd09a3b37211f
zakaria chatouane <yousra.ch.etudiante@gmail.com --global user.name yousra>
2023-12-05 22:46:36 +0100

    error fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ import sys
 if __name__ == "__main__":
     length = len(sys.argv)
     if length == 1:
-        print("{:d} arguments.".format(length - 1))
+        print("{:d} argument.".format(length - 1))
     else:
         print("{:d} arguments:".format(length - 1))
     for i in range(1, length):
