ab1ac408111177b2994222f26705b323176cb378
zakaria chatouane <yousra.ch.etudiante@gmail.com --global user.name yousra>
2023-12-18 23:15:13 +0100

    all tasks

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,7 +3,7 @@ from sys import stderr
 
 def safe_function(fct, *args):
     try:
-        return fct(*args)
+        return(fct(*args))
     except Exception as num:
         stderr.write("Exception: {}\n".format(num))
         return(None)
