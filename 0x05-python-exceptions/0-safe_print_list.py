dfd9ea1f4db244d294691af59e3ffd35540526b5
zakaria chatouane <yousra.ch.etudiante@gmail.com --global user.name yousra>
2023-12-18 22:47:50 +0100

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     printed_elem = 0
     try:
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     nbr_printed = 0
     for i in range(x):
