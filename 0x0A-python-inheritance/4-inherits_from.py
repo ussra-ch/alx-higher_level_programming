1d8507e92aab048d6c3815a354481096d4fcd6dc
zakaria chatouane <yousra.ch.etudiante@gmail.com --global user.name yousra>
2024-02-19 11:37:56 +0100

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""inherits_from function"""
+
+def inherits_from(obj, a_class):
+    """Returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class ; otherwise False."""
+    return type(obj) != a_class and isinstance(obj, a_class)
