f6860453e243933b7b26203ed4a2327ccf8dc44f
zakaria chatouane <yousra.ch.etudiante@gmail.com --global user.name yousra>
2024-02-20 11:17:50 +0100

    task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""script to appends a string at the end of a text file."""
+
+def append_write(filename="", text=""):
+    """Append a string at the end of a text file."""
+    with open(filename, mode='a', encoding='utf-8') as f:
+        return f.write(text)
