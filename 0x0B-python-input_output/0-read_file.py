57d6d7564b70106fe2f4eb9e8382ebb6880b1a7c
zakaria chatouane <yousra.ch.etudiante@gmail.com --global user.name yousra>
2024-02-20 11:15:40 +0100

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A script to read and print the contents of a file."""
+
+def read_file(filename=""):
+    """Read and print the contents of a file."""
+    with open(filename, mode='r', encoding='utf-8') as myfile:
+        print(myfile.read(), end='')
+
