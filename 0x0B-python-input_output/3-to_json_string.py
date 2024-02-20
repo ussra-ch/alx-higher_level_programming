9aea272c06233b71388bdfbc5c6ab79d73897537
zakaria chatouane <yousra.ch.etudiante@gmail.com --global user.name yousra>
2024-02-20 11:18:31 +0100

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""script to return the JSON representation of an object"""
+import json
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object."""
+    return json.dumps(my_obj)
